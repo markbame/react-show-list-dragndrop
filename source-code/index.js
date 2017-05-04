748c19ed2bf648dd75e37cd4b2e23e2997765ff7
markbame <gearsoflife.mark@gmail.com>
2017-05-04 16:27:38 +0800

    format

--- source-code/index.js
+++ source-code/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import {
+  Provider
+}
+from 'react-redux'
 import common from './style/style.scss'
-import { store } from './store/store'
+import {
+  store
+}
+from './store/store'
 import ShowRoute from './route/route'
 
 ReactDOM.render(
   <Provider store={store}>
     <ShowRoute />
-  </Provider>
-, document.querySelector('#container'))
+  </Provider>, document.querySelector(
+    '#container'))
